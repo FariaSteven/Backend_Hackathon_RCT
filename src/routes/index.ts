b437f9404ce772c89058bc36209bdc563f0a2634
Augusto Calixto <gutodenovaes@outlook.com>
2020-10-25 16:24:20 -0300

    Revert "funcionalidades basicas da api com sqlite3"
    This reverts commit 13c5d849a82e7d013b879ee5529a94998fbf422f.

--- src/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import formularioRouter from './formulario.routes';
-const routes = Router();
-
-routes.use('/formulario', formularioRouter)
-
-export default routes;
